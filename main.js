bad6d7c0e2f842747ccea7597a89581e1624ec79
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2020-12-02 15:12:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-var pizzaarry=["mushroom pizza","Vegetable pizza","Pizza candyland","PIzza fruit"];
+var pizzaarry=["Mushroom pizza","Vegetable pizza","Pizza candyland","Pizza fruit"];
 function getmenu(){
     var display=[];
     for(var i=0; i<pizzaarry.length;i++){
         display.push("<h4> "+pizzaarry[i]+"</h4>");
+
     }
+    pizzaarry.sort();
     var removecommas=display.join(" ");
 document.getElementById("menu").innerHTML=removecommas;
 }
 function add_item(){
     var newpizza=document.getElementById("add_item").value;
     pizzaarry.push(newpizza);
+    pizzaarry.sort();
     document.getElementById("add_item").value="";
 }
+
